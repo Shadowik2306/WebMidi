f44594b37be9405a5fc8428e649110c4828b4902
shadowik <zahar.mark@bk.ru>
2023-05-29 18:50:51 +0400

    ssl

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const fs = require('fs')
+module.exports = {
+  devServer: {
+    host: 'dev.local',
+    port: 8080,
+    https: {
+      key: fs.readFileSync('./certs/dev.local+4-key.pem'),
+      cert: fs.readFileSync('./certs/dev.local+4.pem'),
+      //ca: fs.readFileSync('./certs/my-ca.crt')
+    },
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:1337',
+        changeOrigin: true
+      },
+    }
+  }
+}
